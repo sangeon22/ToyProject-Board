eef9992465c09bafa338b3cf6f23fa957edbd442
sangeon22 <tkddjsdl33@hanmail.net>
2022-05-16 15:50:08 +0900

    Initial commit

---
+++ src/main/java/com/study/toyprojectboardspringboot/ToyProjectBoardSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.study.toyprojectboardspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectBoardSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectBoardSpringBootApplication.class, args);
+    }
+
+}
